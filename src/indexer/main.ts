91844b01f938175e109f21433fa065194e10311a
Lover Atomical <khahare0@gmail.com>
2024-02-28 21:49:50 +0545

    added logs

--- src/indexer/main.ts
+++ src/indexer/main.ts
@@ -1,9 +1,12 @@
 import IndexerQuery from "../shared/database/query-indexer";
+import Logger from "../shared/system/logger";
 import BlockIndexer from "./block-indexer/Block-Indexer";
 import inscriptionIndex from "./ordinals-inscriptions/Inscription-indexer";
 
 const DoginalsIndexer = async () => {
   //Load the indexer data
+
+  Logger.Success(`Starting doginals indexer....`);
 
   const IndexerStatus = await IndexerQuery.LoadIndexerStatus();
 
@@ -17,6 +20,10 @@ const DoginalsIndexer = async () => {
 
   let currentInscriptioNumber = 0;
 
+  Logger.Success(
+    `Indexer Status:- Inscription Fetcher Block :- ${inscriptionIndexedBlock}, Block Fetcher Block:- ${startBlock}`
+  );
+
   while (1) {
     /**
      *
@@ -26,6 +33,14 @@ const DoginalsIndexer = async () => {
      */
 
     if (TotalBlockIndex >= InscriptionFetchingQue) {
+      const TimerStart = performance.now();
+
+      Logger.Success(
+        `Starting to index inscription from block ${inscriptionIndexedBlock} to ${
+          startBlock - TotalBlockIndex
+        }`
+      );
+
       const inscriptionBlockIndexed = await inscriptionIndex(
         inscriptionIndexedBlock,
         startBlock,
@@ -48,22 +63,45 @@ const DoginalsIndexer = async () => {
 
         TotalBlockIndex = 0;
       }
+      const TimerEnd = performance.now();
+
+      const TimeTaken = ((TimerEnd - TimerStart) / 60).toFixed(3);
+
+      Logger.Success(
+        `Successfully indexed Inscription untill block ${inscriptionIndexedBlock}. Took ${TimeTaken} sec...`
+      );
+
+      Logger.Success(
+        `Out of ${TotalBlockIndex}, ${BlockScanned} blocks has been scanned`
+      );
 
       await IndexerQuery.UpdateLastInscriptionIndexedBlock(
         inscriptionIndexedBlock
       );
       continue;
     }
+    const TimerStart = performance.now();
+
+    Logger.Success(`Indexing Block Transaction from ${startBlock}....`);
 
     const NextBlock = await BlockIndexer(startBlock);
 
+    Logger.Success(`Updating block state `);
+
     await IndexerQuery.UpdateLastTransactionIndexedBlock(NextBlock.nextBlock); //nextstarting block
 
-    TotalBlockIndex = NextBlock.scanRange;
+    TotalBlockIndex += NextBlock.scanRange;
 
     await IndexerQuery.UpdateTotalBlockIndex(TotalBlockIndex);
 
+    const TimerEnd = performance.now();
+
+    const TimeTaken = ((TimerEnd - TimerStart) / 60).toFixed(3);
+
     //Update the next block
+    Logger.Success(
+      `Successfully Index Block till ${NextBlock.nextBlock}. Took ${TimeTaken} sec...`
+    );
 
     startBlock = NextBlock.nextBlock;
   }
